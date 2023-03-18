3be7391e6ca3369aae20d5ea0bba6dd423b0f4e5
Vidit Kushwaha <viditkushwaha530@gmail.com>
2023-03-19 01:27:20 +0530

    d1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ function Navbar() {
       <div className="container-fluid">
     <Link className="navbar-brand" to="/">
       {/* <img src="../../public/ino" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"/> */}
-      iNotes</Link>
+      Notebook</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
